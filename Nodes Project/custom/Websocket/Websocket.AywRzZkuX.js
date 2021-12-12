fb31c94609647c771f98b72075d95a60a2ec15b9
Adam Gilbert <spacetimedecor@gmail.com>
2021-12-12 19:00:31 +0000

    More and better

---
+++ Nodes Project/custom/Websocket/Websocket.AywRzZkuX.js
@@ -0,0 +1,54 @@
+
+
+module.exports = (node, graph) => {
+
+  // Defaults 
+  const defaultListeners = { 
+    default: ({ type, args, meta }) => {
+      debug && console.log(`Server ${type} received:`, args, meta); 
+    },
+  }
+
+  // Global settings:
+  graph.webSocket = null;
+  graph.debug = false;
+  graph.wbListeners = defaultListeners;
+
+  // Global imports
+  let { debug, wbListeners, webSocket } = graph;
+
+  // On node ready:
+  node.onReady = () => {
+
+    // Create websocket
+    webSocket = new WebSocket("ws://localhost:8888");
+    graph.webSocket = webSocket;
+
+    // On node connected to:
+    webSocket.onopen = () => {
+      debug && console.log('New client connection opened! Saying hello to server.');
+
+      // Say hello to server:
+      webSocket.send(JSON.stringify({ 
+        type: 'message', 
+        args: 'Hello server! Im new client', 
+        meta: {}
+      }));
+    };
+
+    // On node connected to:
+    webSocket.onmessage = (msg) => {
+      const decodedMessage = JSON.parse(msg.data); 
+
+      Object.values(wbListeners)
+        .forEach(listener => listener(decodedMessage))
+    }
+  };
+
+  // On node destroy:
+  node.onDestroy = () => {
+    debug && console.log('Node destroyed');  
+    webSocket.close();
+    debug && console.log('Socket closed');    
+  };
+};
